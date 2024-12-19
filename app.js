7456cb3492456ed34aabea2fcc213d6a7b22506a
hannad <hannadalsindi@gmail.com>
2024-12-19 10:22:01 +0300

    add css file

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 3000
 const mongoose = require('mongoose');
 app.set('view engine', 'ejs')
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.render("index")
